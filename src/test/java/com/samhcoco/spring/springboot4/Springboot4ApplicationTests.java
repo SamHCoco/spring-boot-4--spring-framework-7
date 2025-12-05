0ff6628dde3f82c56b59eec89067653d5db83153
Samuel Coco <Samuel.HenriAC@outlook.com>
2025-12-05 17:51:22 +0000

    Project initialised

---
+++ src/test/java/com/samhcoco/spring/springboot4/Springboot4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samhcoco.spring.springboot4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
