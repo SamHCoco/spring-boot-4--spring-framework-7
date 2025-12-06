7ea94f0e4e97b27f394497205e998eb48996a14f
Samuel Coco <Samuel.HenriAC@outlook.com>
2025-12-06 18:23:45 +0000

    API versioning feature

---
+++ README.md
@@ -0,0 +1,6 @@
+## Spring Boot 4 (Spring Framework 7)
+Sandbox project for experimenting with the features of Spring 7 and Spring Boot 4.
+
+### Official Release Notes
+
+https://github.com/spring-projects/spring-framework/wiki/Spring-Framework-7.0-Release-Notes
---
+++ src/main/java/com/samhcoco/spring/springboot4/controller/ApiVersioningController.java
@@ -0,0 +1,28 @@
+package com.samhcoco.spring.springboot4.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class ApiVersioningController {
+
+    @GetMapping(path = "/test-v1", version = "1")
+    public ResponseEntity<?> v1Api() {
+        return ResponseEntity.ok("v1 API (default setting)");
+    }
+
+    @GetMapping(path = "/test-v2", version = "2")
+    public ResponseEntity<?> v2Api() {
+        return ResponseEntity.ok("v2 API");
+    }
+
+    @GetMapping(path = "/test-v2.2+", version = "2.2+")
+    public ResponseEntity<?> v2Point2PlusApi() {
+        return ResponseEntity.ok("v2.2+ API (+ indicates 2.2 or above)");
+    }
+
+
+}
